2b71d7dac837fa2e3007fee5706b0a19489c5d6d
williams-esthera <williams.esthera@gmail.com>
2025-03-12 20:52:06 -0400

    upload card images

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Calc App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'My Calc App'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,69 +30,155 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Color background = Color(0xFFC9CBA3);
+  Color background2 = Color(0xFFFFE1A8);
+  Color accent1 = Color(0xFFA9C0D38);
+  Color accent2 = Color(0xFF4C86A8);
+  Color text = Color(0xFF1C1D21);
+  Color text2 = Color.fromARGB(255, 250, 254, 252);
+  List grid1 = ['card1','card2','card3'];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+//checks edge cases for enter button
+  void method() {
+
   }
+
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: background2,
         title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        titleTextStyle: TextStyle( // Your custom font
+          fontSize: 50,
+          fontWeight: FontWeight.bold,
+          color: text,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: background,
+      body: Column(
+        mainAxisAlignment:
+            MainAxisAlignment.start, // Align all children to the start
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // First Expanded for Calculations + Error messages
+          Expanded(
+            child: Container(
+              color: background2,
+              child: SizedBox(
+                height: 200,
+                width: double.infinity,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  Container(
+                    width: 450,
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: accent1,width: 3),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  ],
+                ),
+                ),
+              ),
+            ),
+          ),
+
+          // for Numbers, Clear button, and Equal button
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 70.0,
+              ), // Add vertical padding
+              child: SizedBox(
+                height: 100,
+                width: double.infinity,
+                child: Center(
+                  child: GridView.builder(
+                    itemCount: grid1.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      childAspectRatio: 1,
+                      mainAxisSpacing: 8, // Space between rows
+                      crossAxisSpacing: 8, // Space between cols
+                    ),
+
+                    itemBuilder: (context, index) {
+                      return Container(
+                        height: 5,
+                        width: 5,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: accent1,
+                          ),
+                          onPressed: () {
+                            _gridOneOperators(grid1[index]);
+                          },
+                          child: Center(
+                            child: Text(
+                              grid1[index],
+                              style: TextStyle(fontSize: 50, color: text),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          //Grid for operators 
+          Container(
+            height: 200, // Set the height as needed
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 20.0,
+                horizontal: 20.0,
+              ),
+              child: Center(
+                child: GridView.builder(
+                  itemCount: grid2.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    childAspectRatio: 1,
+                    mainAxisSpacing: 8, // Space between rows
+                    crossAxisSpacing: 8, // Space between cols
+                  ),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 5,
+                      width: 5,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: accent2,
+                        ),
+                        onPressed: () {
+                          _gridTwoOperators(grid2[index]);
+                        },
+                        child: Center(
+                          child: Text(
+                            grid2[index],
+                            style: TextStyle(fontSize: 50, color: text2),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
